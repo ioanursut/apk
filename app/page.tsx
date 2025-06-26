fc501f7e63c116ca4c9c80be120e992e33484f38
ioanursut <johan.olsson82@gmail.com>
2025-06-26 08:17:18 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Solöl 🍺</h1>
+      <p>Här kommer karta + lista över billig öl i närheten.</p>
+    </main>
+  );
+}
